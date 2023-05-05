6fed1f88e1783e3dab276fbbdd49b9e4f344cc00
JuLealB <94349598+JuLealB@users.noreply.github.com>
2023-05-05 20:48:10 -0300

    Add files via upload

---
+++ App-Filmes-main/App-Filmes-main/src/components/bannerMovies/styles.js
@@ -0,0 +1,18 @@
+import {StyleSheet} from "react-native"
+
+const styles = StyleSheet.create({
+    textBanner:{
+        fontSize: 30,
+        color: "white",
+        marginTop: 15,
+        fontWeight: "Bold"
+    },
+
+    imageBanner:{
+        width: '85%',
+        height: 200,
+        marginTop: 15,
+        borderRadius: 10
+    }
+})
+export default styles;
---
+++ App-Filmes-main/App-Filmes-main/src/data/filmes.js
@@ -0,0 +1,74 @@
+const DATA =
+[
+    {
+        id:"01",
+        nome:"Adão Negro",
+        notas:'8,5',
+        imagem:'adao.jpg'
+    },
+
+    {
+        id:"02",
+        nome:"Avatar",
+        notas:'6,8',
+        imagem:'avatar.jpg'
+    },
+
+    {
+        id:"03",
+        nome:"Desaparecida",
+        notas:'7,5',
+        imagem:'desaparecida.jpg'
+    },
+
+    {
+        id:"04",
+        nome:"Gato de Botas II",
+        notas:'9,9',
+        imagem:'gato.jpg'
+    },
+
+    {
+        id:"05",
+        nome:"Homem Formiga e a Vespa",
+        notas:'8,5',
+        imagem:'homemformiga.jpg'
+    },
+
+    {
+        id:"06",
+        nome:"Incriveis",
+        notas:'8,2',
+        imagem:'incriveis.jpg'
+    },
+
+    {
+        id:"07",
+        nome:"Mario",
+        notas:'8,7',
+        imagem:'mario.jpg'
+    },
+
+    {
+        id:"08",
+        nome:"Megan",
+        notas:'7,8',
+        imagem:'megan.jpg'
+    },
+
+    {
+        id:"09",
+        nome:"Pânico",
+        notas:'8,4',
+        imagem:'panico.jpg'
+    },
+
+    {
+        id:"10",
+        nome:"Wandinha",
+        notas:'4,2',
+        imagem:'wandinha.jpg'
+    }
+]
+
+export default DATA;
